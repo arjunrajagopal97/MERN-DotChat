901db4df8426791055bda30b14e5eb6f5da26f40
arjun <arjunrajagopal97@gmail.com>
2024-07-29 23:13:41 +0530

    First Commit

---
+++ src/pages/HomePages.js
@@ -0,0 +1,73 @@
+import {
+  Container,
+  Box,
+  Text,
+  Tabs,
+  Tab,
+  TabList,
+  TabPanels,
+  TabPanel,
+} from "@chakra-ui/react";
+import React, { useEffect } from "react";
+import "../App.css";
+import Login from "../components/Authentication/Login";
+import SignUp from "../components/Authentication/SignUp";
+import { useNavigate } from "react-router-dom";
+const HomePage = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+    if (!userInfo) {
+      navigate("/chats");
+    }
+  }, [navigate]);
+  return (
+    <Container maxW="xl" centerContent>
+      <Box
+        display="flex"
+        justifyContent="center"
+        p={3}
+        bg={"white"}
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text
+          fontSize="4xl"
+          textAlign="center"
+          fontFamily="Work sans"
+          color="black"
+        >
+          Talk-with-Me
+        </Text>
+      </Box>
+      <Box
+        bg="white"
+        w="100%"
+        p={4}
+        borderRadius="lg"
+        color="black"
+        borderWidth="1px"
+      >
+        <Tabs variant="soft-rounded" colorScheme="blue">
+          <TabList mb="1em">
+            <Tab width="50%">Login</Tab>
+            <Tab width="50%">Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <SignUp />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
+};
+
+export default HomePage;
