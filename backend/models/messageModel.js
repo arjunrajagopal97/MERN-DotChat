ac307901b35991b0777efb7305908dff061eb045
arjunrajagopal97 <arjunrajagopal97@gmail.com>
2024-09-29 11:57:20 +0530

    inital
    File changes

---
+++ backend/models/messageModel.js
@@ -0,0 +1,16 @@
+const { timeStamp } = require("console");
+const mongoose = require("mongoose");
+
+const messageSchema = mongoose.Schema(
+  {
+    sender: { type: mongoose.Types.ObjectId, ref: "User" },
+    content: { type: String, trim: true },
+    chat: { type: mongoose.Types.ObjectId, ref: "Chat" },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Message = mongoose.model("Message", messageSchema);
+module.exports = Message;
---
+++ backend/server.js
@@ -0,0 +1,98 @@
+const express = require("express");
+const { chats } = require("./data/data");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+const chatRoutes = require("./routes/chatRoutes");
+const messageRoutes = require("./routes/messageRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+const path = require("path");
+dotenv.config();
+connectDB();
+const app = express();
+
+app.use(express.json());
+app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
+
+// --------------------------deployment------------------------------
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+// dummy
+
+// --------------------------deployment------------------------------
+
+app.use(notFound);
+app.use(errorHandler);
+const port = process.env.PORT || 5000;
+
+const server = app.listen(
+  5000,
+  console.log(`Server listening on port ${port}`)
+);
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "http://localhost:3000",
+    // credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("Connected to socket.io");
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
+    socket.emit("connected");
+  });
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("User Joined Room: " + room);
+  });
+  socket.on("typing", (room) => socket.in(room).emit("typing"));
+  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
+  socket.on("new message", (newMessageRecieved) => {
+    var chat = newMessageRecieved.chat;
+
+    if (!chat.users) return console.log("chat.users not defined");
+
+    chat.users.forEach((user) => {
+      if (user._id == newMessageRecieved.sender._id) return;
+
+      socket.in(user._id).emit("message recieved", newMessageRecieved);
+    });
+  });
+  // Handle signaling data
+  // socket.on("signal", (data) => {
+  //   const { to, signalData } = data;
+  //   io.to(to).emit("signal", signalData);
+  // });
+
+  // Handle joining a call room
+  socket.on("join-call", (roomId) => {
+    socket.join(roomId);
+    socket.broadcast.to(roomId).emit("user-joined", socket.id);
+  });
+
+  // Handle user disconnect
+  // socket.on("disconnect", () => {
+  //   console.log("Client disconnected");
+  // });
+  socket.off("setup", () => {
+    console.log("USER DISCONNECTED");
+    socket.leave(userData._id);
+  });
+});
